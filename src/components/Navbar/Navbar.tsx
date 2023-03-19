4ca180c3f951595f965e5f449a0dcaf05288b409
iljah <ilja200303@gmail.com>
2023-03-19 14:10:11 +0200

    navbar code refactoring"

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import './navbar.scss'
 import logo from "../../assets/logo.svg";
+import {FiMenu} from "react-icons/fi"
+import {useState, useEffect} from "react";
+import {RiCloseLine} from "react-icons/ri"
+
 function Navbar() {
+    const [toggle, setToggle] = useState<boolean>(false);
+    useEffect(() => {
+        console.log(toggle)
+    }, [toggle])
     return (
         <div className="navbar">
            <div className="container">
-               <div className="menu">
 
+               <div className="logo">
+                   <img src={logo} alt="Logo"/>
+               </div>
+
+               <FiMenu size={40} className="burger" onClick={() => setToggle(prevState => !prevState)}/>
+
+
+               <div className="menu" id={toggle ? "toggle": ""}>
+
+                   <RiCloseLine size={40} className="close" onClick={() => setToggle(prevState => !prevState)}/>
                    <ul>
-                       <li className="logo">
-                           <img src={logo} alt="Logo"/>
-                       </li>
                        <li>
                            <a href="/">Buy</a>
                        </li>
