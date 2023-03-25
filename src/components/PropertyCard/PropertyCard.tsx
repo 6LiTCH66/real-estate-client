9a8ff1a9009fdbfbad7154514a55fe03afbdfb75
iljah <ilja200303@gmail.com>
2023-03-25 11:00:04 +0200

    some changes in top offers sections

--- src/components/PropertyCard/PropertyCard.tsx
+++ src/components/PropertyCard/PropertyCard.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import "./propertyCard.scss"
+import {AiOutlineHeart} from "react-icons/ai"
+import {BsHeart} from "react-icons/bs"
 function PropertyCard() {
     const [price, setPrice] = useState<number>(3290000)
     const [sqft, setSqft] = useState<number>(1200)
@@ -8,11 +10,21 @@ function PropertyCard() {
         <div className="card">
             <div className="container">
                 <div className="image">
+                    <div className="status">
+                        <p>
+                            For Sale
+                        </p>
+                    </div>
                     <img src="https://photos.zillowstatic.com/fp/f6a50baf44ca9e011448f5bf228c7794-cc_ft_960.jpg" alt=""/>
+                    <div className="favourite">
+                        <AiOutlineHeart size={27}/>
+                    </div>
                 </div>
                 <div className="body">
 
                     <p className="price">${price.toLocaleString()}</p>
+                    <address>2679 Syracuse Court, Denver, Colorado 80238</address>
+
                     <div className="data">
                         <p>
                             <strong>3</strong> bed
@@ -23,16 +35,20 @@ function PropertyCard() {
                         <p>
                             <strong>{sqft.toLocaleString()}</strong> sqft
                         </p>
-                    </div>
-                    <address>2679 Syracuse Court, Denver, Colorado 80238</address>
+                        <p>
+                            Apartment
+                        </p>
 
-                    <hr/>
-                    <div className="footer">
-                        <div className="image-wrapper">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFYxcol9EhezDIhNK3QE-8N_2NsI5gH3zgkOQxlGs-TyCIsEK30Acavt8l11q9vldk0wA&usqp=CAU" alt=""/>
-                        </div>
-                        <p>Olivia Johnson</p>
+
                     </div>
+
+                    {/*<hr/>*/}
+                    {/*<div className="footer">*/}
+                    {/*    <div className="image-wrapper">*/}
+                    {/*        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFYxcol9EhezDIhNK3QE-8N_2NsI5gH3zgkOQxlGs-TyCIsEK30Acavt8l11q9vldk0wA&usqp=CAU" alt=""/>*/}
+                    {/*    </div>*/}
+                    {/*    <p>Olivia Johnson</p>*/}
+                    {/*</div>*/}
                 </div>
 
             </div>
