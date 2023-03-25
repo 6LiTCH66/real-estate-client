92e90efa2b14ed3a1e60bee8530b4e3ae05d3ad9
iljah <ilja200303@gmail.com>
2023-03-25 16:31:52 +0200

    slider on click and some other futures were added to top offers sections

--- src/components/PropertyCard/PropertyCard.tsx
+++ src/components/PropertyCard/PropertyCard.tsx
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
 import "./propertyCard.scss"
-import {AiOutlineHeart} from "react-icons/ai"
-import {BsHeart} from "react-icons/bs"
+import {AiOutlineHeart, AiFillHeart} from "react-icons/ai"
+
 function PropertyCard() {
     const [price, setPrice] = useState<number>(3290000)
     const [sqft, setSqft] = useState<number>(1200)
+    const [favouriteClick, setFavouriteClick] = useState<boolean>(false)
+
 
     return (
         <div className="card">
@@ -16,8 +18,8 @@ function PropertyCard() {
                         </p>
                     </div>
                     <img src="https://photos.zillowstatic.com/fp/f6a50baf44ca9e011448f5bf228c7794-cc_ft_960.jpg" alt=""/>
-                    <div className="favourite">
-                        <AiOutlineHeart size={27}/>
+                    <div className="favourite" onClick={() => setFavouriteClick(prevState => !prevState)}>
+                        <AiFillHeart size={27} color={favouriteClick ? '#E83845' : '#141B2D'}/>
                     </div>
                 </div>
                 <div className="body">
@@ -41,14 +43,6 @@ function PropertyCard() {
 
 
                     </div>
-
-                    {/*<hr/>*/}
-                    {/*<div className="footer">*/}
-                    {/*    <div className="image-wrapper">*/}
-                    {/*        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFYxcol9EhezDIhNK3QE-8N_2NsI5gH3zgkOQxlGs-TyCIsEK30Acavt8l11q9vldk0wA&usqp=CAU" alt=""/>*/}
-                    {/*    </div>*/}
-                    {/*    <p>Olivia Johnson</p>*/}
-                    {/*</div>*/}
                 </div>
 
             </div>
--- src/components/TopOffers/TopOffers.tsx
+++ src/components/TopOffers/TopOffers.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import "./topOffers.scss"
 import {BsArrowRightShort, BsArrowLeftShort} from "react-icons/bs"
 import {PropertyCard} from "../index";
 
 function TopOffers() {
+    const scrollRef = useRef<HTMLDivElement>(null)
+    const [scrolledToEnd, setScrollToEnd] = useState<boolean>(false);
+    const [scrolledToStart, setScrollToStart] = useState<boolean>(true);
+
+    const scroll = (direction: "left" | "right") => {
+        const container = scrollRef.current;
+
+        if(container){
+            const scrollWidth = container.scrollWidth
+
+            switch (direction){
+                case "left":
+                    container.scrollLeft -= scrollWidth / 3
+                    break;
+                case "right":
+                    container.scrollLeft += scrollWidth / 3
+                    break;
+            }
+
+        }
+    }
+
+    const handleScroll = () => {
+        const container = scrollRef.current;
+        if (container){
+            const { scrollLeft, scrollWidth, clientWidth } = container;
+            if (scrollLeft >= scrollWidth - clientWidth){
+                setScrollToEnd(prevState => !prevState)
+            }else{
+                setScrollToEnd(false)
+            }
+
+            if (scrollLeft === 0){
+                setScrollToStart(prevState => !prevState)
+            }else{
+                setScrollToStart(false)
+            }
+        }
+    }
+
     return (
         <div className="top-offers">
             <div className="container">
@@ -25,48 +65,25 @@ function TopOffers() {
 
                 <div className="scroll-wrapper">
 
-                    <button>
+                    <button type="button" onClick={() => scroll("left")} style={{backgroundColor: scrolledToStart ? "#DADAEE": "", cursor: !scrolledToStart ? "pointer": "default"}}>
                         <BsArrowLeftShort color="white" size={42} className="btn-arrow"/>
                     </button>
 
-                    <button>
+                    <button type="button" onClick={() => scroll("right")} style={{backgroundColor: scrolledToEnd ? "#DADAEE": "", cursor: !scrolledToEnd ? "pointer": "default"}}>
                         <BsArrowRightShort color="white" size={42} className="btn-arrow"/>
                     </button>
                 </div>
 
 
-                <div className="slider">
+                <div className="slider" ref={scrollRef} onScroll={handleScroll}>
                     <PropertyCard/>
                     <PropertyCard/>
                     <PropertyCard/>
                     <PropertyCard/>
                     <PropertyCard/>
                     <PropertyCard/>
-
-
-                    {/*<div className="card">*/}
-
-                    {/*</div>*/}
-
-                    {/*<div className="card">*/}
-
-                    {/*</div>*/}
-
-                    {/*<div className="card">*/}
-
-                    {/*</div>*/}
-
-                    {/*<div className="card">*/}
-
-                    {/*</div>*/}
-
-                    {/*<div className="card">*/}
-
-                    {/*</div>*/}
-
-                    {/*<div className="card">*/}
-
-                    {/*</div>*/}
+                    <PropertyCard/>
+                    <PropertyCard/>
                 </div>
 
             </div>
